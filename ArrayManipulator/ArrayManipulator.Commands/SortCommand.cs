765321cd0f82b3dcc84f97f541d81425f540f539
cecube <cecubebg@gmail.com>
2017-11-20 00:21:01 +0200

    Created a new command - SortCommand

--- ArrayManipulator/ArrayManipulator.Commands/SortCommand.cs
+++ ArrayManipulator/ArrayManipulator.Commands/SortCommand.cs
@@ -10,14 +10,41 @@ namespace ArrayManipulator.Commands
         public SortCommand(string[] arrayToManipulate) : base(arrayToManipulate)
         { }
 
-        protected override IArrayCommandResult ManipulateTheArray(string[] arrayToManipulate)
-        {
-            throw new NotImplementedException();
+        protected override void ValidateCommandParamaters()
+        {           
         }
 
-        protected override void ValidateCommandParamaters()
+        protected override IArrayCommandResult ManipulateTheArray(string[] arrayToManipulate)
         {
-            throw new NotImplementedException();
+
+            for(int i = 1; i < arrayToManipulate.Length;)
+            {
+                string currentWord = arrayToManipulate[i];
+                string previousWord = arrayToManipulate[i - 1];
+
+                if (currentWord[0] >= previousWord[0])
+                {
+                    i++;
+                }
+                else
+                {
+                    string tempValue = previousWord;
+
+                    arrayToManipulate[i - 1] = currentWord;
+                    arrayToManipulate[i] = tempValue;
+
+                    i--;
+
+                    if(i == 0)
+                    {
+                        i = 1;
+                    }
+                }
+            }
+
+            string sortedText = string.Join(" ", arrayToManipulate);
+
+            return new ArrayCommandResult(sortedText, arrayToManipulate);
         }
     }
 }
