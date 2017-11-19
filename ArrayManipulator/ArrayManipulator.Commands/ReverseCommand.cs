fe92545151ff3bb3d310969c831e1438bb842fc2
Plotso <ilian_1999n@abv.bg>
2017-11-20 01:42:09 +0200

    Implemented the ReverseCommand

---
+++ ArrayManipulator/ArrayManipulator.Commands/ReverseCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace ArrayManipulator.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ArrayManipulator.Commands.CommandResult.Interfaces;
+
+    public class ReverseCommand : ArrayCommand
+    {
+        public ReverseCommand(string[] arrayToManipulate) 
+            : base(arrayToManipulate)
+        {
+        }
+
+        protected override IArrayCommandResult ManipulateTheArray(string[] arrayToManipulate)
+        {
+            Array.Reverse(arrayToManipulate);
+            string message = string.Join(" ", arrayToManipulate);
+            return new ArrayCommandResult(message, arrayToManipulate);
+        }
+
+        protected override void ValidateCommandParamaters()
+        {
+        }
+    }
+}
