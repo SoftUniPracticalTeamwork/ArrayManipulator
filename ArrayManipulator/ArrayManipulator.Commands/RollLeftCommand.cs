72d6650f76f43a40e4c298b7172cfc1cfceb3ec5
Plotso <ilian_1999n@abv.bg>
2017-11-20 02:01:56 +0200

    Implemented the RollLeftCommand -> The easiest way to roll an array

---
+++ ArrayManipulator/ArrayManipulator.Commands/RollLeftCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayManipulator.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ArrayManipulator.Commands.CommandResult.Interfaces;
+
+    public class RollLeftCommand : ArrayCommand
+    {
+        public RollLeftCommand(string[] arrayToManipulate)
+            : base(arrayToManipulate)
+        {
+        }
+
+        protected override IArrayCommandResult ManipulateTheArray(string[] arrayToManipulate)
+        {
+            //easy way
+            string[] arr = arrayToManipulate.Skip(1).Concat(arrayToManipulate.Take(1)).ToArray();
+            
+            string message = string.Join(" ", arr);
+            return new ArrayCommandResult(message, arrayToManipulate);
+        }
+
+        protected override void ValidateCommandParamaters()
+        {
+        }
+    }
+}
